0d07f31a9ca0af72ba70be1489a971fce6c9856e
hyou0064 <vthpjn@gmail.com>
2023-04-19 17:11:10 +0900

    button update

--- program/game/gm_button.h
+++ program/game/gm_button.h
@@ -8,14 +8,33 @@ public:
 	//void update();
 	Button() {}
 	~Button() {}
+
+	enum class Type {
+		Dungeon,
+		Chara,
+		shop,
+		storate
+	};
+
+
 	
 	void UpdateMousePoint(int x, int y) { mouse_point_x_ = x; mouse_point_y_ = y; }
 	bool IsBoundsLeftClicked(int x1, int y1, int x2, int y2);
+	bool IsLeftClicked();
 
 private:
 
-	int mouse_point_x_ = 0;
-	int mouse_point_y_ = 0;
+	int mouse_point_x_;
+	int mouse_point_y_;
+
+	//ボタンを配置するpos,左上頂点座標からw,hの大きさに展開
+	int pos_x_ = 0;
+	int pos_y_ = 0;
+
+	int size_w_ = 0;
+	int size_h_ = 0;
+
+
 
 
 
--- program/game/ui/ui_button.h
+++ program/game/ui/ui_button.h
@@ -5,6 +5,11 @@ class Button : public UIComponent {
 public:
 	void Draw() = 0;
 
-
+private:
+	
+	int size_x_ = 0;
+	int size_h_ = 0;
+	int pos_x_ = 0;
+	int pos_y_ = 0;
 
 };
